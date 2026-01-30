a5446eafd39d7438180165604673fea472f45df6
Vinothini Ramakrishna <145571986+vinothinikrishnan24@users.noreply.github.com>
2026-01-30 08:38:41 +0530

    first commit

---
+++ java.js
@@ -0,0 +1,50 @@
+// 11.var a=10;
+// a=20;
+// var a=30;
+// console.log(a);
+
+
+
+//12. let b = 5;
+// b=15;
+// console.log(b);
+
+
+//13. let x =10;
+// let x =20; SyntaxError: Identifier 'x' has already been declared
+
+//14. const y=50;
+// y=100; TypeError: Assignment to constant variable.
+
+
+// 15.var z=70;
+// var z=80;
+// console.log("var redeclaration:-",z);
+
+// 16.let c=2;
+// c=3;
+// console.log(c);
+
+//17. const d=0;
+// console.log(d);
+
+// before 
+//  var age = 5;
+//after
+let a=3;
+console.log("let:-",a);
+
+
+
+
+//19.const city ="chennai";
+// console.log(city);
+
+// 20.var country="India";
+// let a=3;
+// const pi=3.14;
+// console.log("var:-",country);
+// console.log("let:-",a);
+// console.log("const:-",pi);
+
+
